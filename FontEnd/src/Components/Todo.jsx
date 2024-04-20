940b5fa6e8a775a98858008e1a856adb713095ec
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-04-20 23:40:19 +0600

    Second Commit

---
+++ FontEnd/src/Components/Todo.jsx
@@ -0,0 +1,19 @@
+
+
+const Todo = () => {
+    return (
+        <div>
+            <div className="todo">
+
+                <div>
+                    <div className="logo">
+                        <img className="w-11 h-9"  src="https://static.vecteezy.com/system/resources/thumbnails/003/529/153/small/business-to-do-list-flat-icon-modern-style-free-vector.jpg" alt="" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Todo;
